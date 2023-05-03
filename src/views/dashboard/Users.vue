e98b63963fd460263d5e1b22da9708ec91a678d1
DanRotaru <dan.rotaru2000@gmail.com>
2023-05-04 00:09:42 +0300

    Users list improvements

--- src/views/dashboard/Users.vue
+++ src/views/dashboard/Users.vue
@@ -56,7 +56,9 @@
                     <!-- Title -->
                     <div class="d-sm-flex justify-content-sm-between mb-2 mb-sm-3">
                       <div>
-                        <h5 class="card-title mb-0"><a href="#">DanRotaru</a></h5>
+                        <h5 class="card-title mb-0">
+                          <router-link to="/dashboard/users/id21123">DanRotaru</router-link>
+                        </h5>
                         <p class="small mb-2 mb-sm-0">Fullstack web developer</p>
                       </div>
                       <span class="h6 fw-light">4.3<i class="fas fa-star text-warning ms-1"></i></span>
